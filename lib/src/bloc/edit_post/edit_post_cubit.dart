e526c56ff6ef1a2bb1bcd2a77b74d6d6b62b1307
luciano <desmondangel@gmail.com>
2023-12-10 03:55:19 -0400

    guardado 10-12-23

--- lib/src/bloc/edit_post/edit_post_cubit.dart
+++ lib/src/bloc/edit_post/edit_post_cubit.dart
@@ -1,6 +1,4 @@
 // ignore_for_file: non_constant_identifier_names
-
-import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:respet_app/main.dart';
@@ -26,7 +24,6 @@ class EditPostCubit extends Cubit<EditPostState> {
     required String vacunasMascota,
     required String mascotaID,
     required bool hayImagen,
-
     required final imagenBytes,
     required final imagenTipo,
     required final usuarioID,
@@ -34,38 +31,28 @@ class EditPostCubit extends Cubit<EditPostState> {
   }) async {
     try {
       emit(EditPostLoading());
-
       String pathOriginal = fotoMascota.substring(113);
       String imagenPath = "/$usuarioID/$pathOriginal";
-
       if (hayImagen = true){
-
         await client.storage
         .from("imagenes")
         .updateBinary(imagenPath, imagenBytes, fileOptions: FileOptions(contentType: "imagen/$imagenTipo"));
-
         await client
         .from("created_post_adoption_pet")
         .update({'photo_pet': fotoMascota, 'name_pet': nombreMascota, 'description_pet': descripcionMascota, 'fur_color': colorPelaje, 
                 'weight_pet': pesoMascota, 'age_pet': edadMascota, 'gender_pet': generoMascota, 'location': localidadMascota, 
                 'is_sterilization': esterilizadoMascota, 'size_pet': tamanoMascota, 'specific_care': cuidadoMascota, 'vaccines_pet': vacunasMascota})
         .match({"id_pet_adoption": mascotaID, "id_user_created": usuarioID});
-
       }else{
-
         await client
         .from("created_post_adoption_pet")
         .update({'photo_pet': fotoMascota, 'name_pet': nombreMascota, 'description_pet': descripcionMascota, 'fur_color': colorPelaje, 
                 'weight_pet': pesoMascota, 'age_pet': edadMascota, 'gender_pet': generoMascota, 'location': localidadMascota, 
                 'is_sterilization': esterilizadoMascota, 'size_pet': tamanoMascota, 'specific_care': cuidadoMascota, 'vaccines_pet': vacunasMascota})
         .match({"id_pet_adoption": mascotaID, "id_user_created": usuarioID});
-
       }
-
       emit(EditPostSuccessful());
-
     } catch (e) {
-
       emit(EditPostFailed(e.toString()));
     }
   }
